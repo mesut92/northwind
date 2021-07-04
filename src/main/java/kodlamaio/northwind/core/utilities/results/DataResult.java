5e85e5e197e5ae0e29aae80e5cfdec6538447ae7
mesut <mesuutoruk1903@gmail.com>
2021-07-04 23:51:30 +0300

    7.Ders bitti

---
+++ src/main/java/kodlamaio/northwind/core/utilities/results/DataResult.java
@@ -0,0 +1,18 @@
+package kodlamaio.northwind.core.utilities.results;
+
+public class DataResult<T> extends Result{
+
+    private T data;
+    public DataResult(T data, boolean success, String message) {
+        super(success, message);
+    }
+    public DataResult(T data, boolean success) {
+        super(success);
+    }
+
+    public T getData(){
+        return this.data;
+    }
+
+//    public class SuccessDataResult
+}
--- src/main/java/kodlamaio/northwind/entities/concretes/Product.java
+++ src/main/java/kodlamaio/northwind/entities/concretes/Product.java
@@ -1,16 +1,21 @@
 package kodlamaio.northwind.entities.concretes;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
+@Data
 @Entity
 @Table(name="products")
+@AllArgsConstructor
+@NoArgsConstructor
 public class Product {
 
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="product_id")
     private int id;
 
@@ -29,64 +34,5 @@ public class Product {
     @Column(name="quantity_per_unit")
     private String quantityPerUnit;
 
-    public Product(){}
-
-    public Product(int id, int categoryId, String productName, double unitPrice, short unitsInStock, String quantityPerUnit) {
-        super();
-        this.id = id;
-        this.categoryId = categoryId;
-        this.productName = productName;
-        this.unitPrice = unitPrice;
-        this.unitsInStock = unitsInStock;
-        this.quantityPerUnit = quantityPerUnit;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public int getCategoryId() {
-        return categoryId;
-    }
-
-    public void setCategoryId(int categoryId) {
-        this.categoryId = categoryId;
-    }
-
-    public String getProductName() {
-        return productName;
-    }
-
-    public void setProductName(String productName) {
-        this.productName = productName;
-    }
-
-    public double getUnitPrice() {
-        return unitPrice;
-    }
-
-    public void setUnitPrice(double unitPrice) {
-        this.unitPrice = unitPrice;
-    }
-
-    public short getUnitsInStock() {
-        return unitsInStock;
-    }
-
-    public void setUnitsInStock(short unitsInStock) {
-        this.unitsInStock = unitsInStock;
-    }
-
-    public String getQuantityPerUnit() {
-        return quantityPerUnit;
-    }
-
-    public void setQuantityPerUnit(String quantityPerUnit) {
-        this.quantityPerUnit = quantityPerUnit;
-    }
 
 }
